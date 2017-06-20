f542e3fa5da3ecf10437bbe3515e504604e29c77
moevegas <moevegas@gmail.com>
2017-06-19 21:11:42 -0400

    Add project files.

---
+++ CECv3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CECv3.Startup))]
+namespace CECv3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
